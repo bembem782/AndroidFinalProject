c398def2421893374adbe7cf8ea72c4f86374e20
bembem782 <bembemcabrera@gmail.com>
2017-05-09 16:57:21 +0800

    Add files via upload

---
+++ Tabs/src/com/example/tabs/PersonName.java
@@ -0,0 +1,17 @@
+package com.example.tabs;
+
+public class PersonName {
+
+	private String name;
+	public PersonName(String name) {
+		super();
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}	
+	
+}
